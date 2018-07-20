fd15f26fed4c40bc29fedcb2f2dec6b6424fdaff
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2018-07-20 14:52:12 +0200

    Prepare deprectaing Interop Container

--- src/UserNotFoundException.php
+++ src/UserNotFoundException.php
@@ -2,8 +2,9 @@
 namespace Germania\Users;
 
 use Interop\Container\Exception\NotFoundException;
+use Psr\Container\NotFoundExceptionInterface as PsrNotFoundExceptionInterface;
 
-class UserNotFoundException extends \Exception implements NotFoundException
+class UserNotFoundException extends \Exception implements NotFoundException, PsrNotFoundExceptionInterface
 {
 
 }
